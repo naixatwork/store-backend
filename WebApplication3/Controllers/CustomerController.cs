c9f5c3ed2e3abffc4aa44e62398448822ea4dea7
naixatwork <naixatwork@gmail.com>
2022-09-12 00:11:21 +0430

    everything

---
+++ WebApplication3/Controllers/CustomerController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApplication3;
+using WebApplication3.Models;
+
+[ApiController]
+[Route("[Controller]")]
+public class CustomerController : ControllerBase
+{
+    public databaseContext context;
+
+    public CustomerController(databaseContext contextCopy)
+    {
+        context = contextCopy;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<List<Customer>>> GetAllCustomers()
+    {
+        var CustomersFromDatabase = await context.Customers
+            .Include(customer => customer.Products)
+            .ToListAsync();
+        return Ok(CustomersFromDatabase);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<Customer>> AddNewCustomer(Customer customer)
+    {
+        context.Customers.Add(customer);
+        await context.SaveChangesAsync();
+        var addedCustomer = await context.Customers
+            .Include(customer => customer.Products)
+            .FirstOrDefaultAsync(_customer => _customer.username == customer.username);
+        return Ok(addedCustomer);
+    }
+
+    [HttpPut("{CustomerId}")]
+    public async Task<ActionResult<Customer>> UpdateCustomer(
+        int CustomerId,
+        [FromBody] Product[] newProduct)
+    {
+        var FoundCustomer = await context.Customers
+            .FindAsync(CustomerId);
+
+        if (FoundCustomer == null)
+        {
+            return NotFound("Existential Crisis ");
+        }
+
+        FoundCustomer.Products = newProduct;
+        await context.SaveChangesAsync();
+        return Ok();
+    }
+
+    [HttpPost("login")]
+    public async Task<ActionResult<Customer>> SignUp(string username, string password)
+    {
+        var FoundCustomer = await context.Customers
+            .Include(customer => customer.Products)
+            .FirstOrDefaultAsync(
+            customer => (customer.username == username && customer.password == password)
+        );
+        if (FoundCustomer == null)
+        {
+            return NotFound("Existential Crisis");
+        }
+        
+        return Ok(FoundCustomer);
+    }
+}
---
+++ WebApplication3/Program.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication3;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors();
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<databaseContext>(options => options.UseSqlite("Name=Sqlite"));
+
+var app = builder.Build();
+
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseHttpsRedirection();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.UseCors(builder => builder
+    .AllowAnyOrigin()
+    .AllowAnyMethod()
+    .AllowAnyHeader());
+
+app.Run();
